8823fb2f759708e4fa96c4e3207ee07a0c26817d
todiez <tobias@diez.cc>
2024-09-23 19:39:10 +0200

    bug fixes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   const countingUp = () => {
-    //const count =  counter + 1;
-    setCounter(counter + 1);
+    setCounter((count) => count + 1);
   };
 
   const countingDown = () => {
-    //const count =  counter + 1;
-    setCounter(counter - 1);
+    setCounter((count) => count - 1);
   };
 
   return (
     <div className="App">
-      
       <h1>Testing stuff</h1>
-      <button onClick={() => countingDown()}>Decrease Number</button>
+      <button onClick={countingDown}>Decrease Number</button>
       <p>{counter}</p>
       <button onClick={() => countingUp()}>Increase Number</button>
     </div>
