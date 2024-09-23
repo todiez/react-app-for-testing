54a067ff446fa137d591ca487bc71f7b0aa77cc2
todiez <tobias@diez.cc>
2024-09-23 19:26:55 +0200

    counter implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">Testing stuff</div>;
+  const [counter, setCounter] = useState(0);
+
+  const countingUp = () => {
+    //const count =  counter + 1;
+    setCounter(counter + 1);
+  };
+
+  const countingDown = () => {
+    //const count =  counter + 1;
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div className="App">
+      
+      <h1>Testing stuff</h1>
+      <button onClick={() => countingDown()}>Decrease Number</button>
+      <p>{counter}</p>
+      <button onClick={() => countingUp()}>Increase Number</button>
+    </div>
+  );
 }
 
 export default App;
