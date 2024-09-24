cbfe17410f501d0a1f64a04f02baf7f640549f26
todiez <tobias@diez.cc>
2024-09-24 14:49:52 +0200

    explanations added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  //if the state changes, the site will be re rendered
+  //if initial state value is hard coded or invokes (calls) a function then it will be run everytime the component is rendered
+  //if initial state value is a function version, it will be run just the first time the component is rendered (useful if heavy cpu load)
+  //example function version: useState(() => countInital())
 
   const countingUp = () => {
     setCounter((count) => count + 1);
+    // count + 1 --> works as well, but only if invoked once in this arrow function
+    // if invoked twice it will be fired only once, with the function it will be fired twice
   };
 
   const countingDown = () => {
