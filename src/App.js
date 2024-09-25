afdfe56398f028113e5d40fd1ea3b9dbe9e81856
todiez <tobias@diez.cc>
2024-09-25 15:31:06 +0200

    useEffect dependency added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("Tobi");
   //if the state changes, the site will be re rendered
   //if initial state value is hard coded or invokes (calls) a function then it will be run everytime the component is rendered
   //if initial state value is a function version, it will be run just the first time the component is rendered (useful if heavy cpu load)
@@ -21,9 +22,13 @@ function App() {
   useEffect(() => {
     //use effect is a hook that runs a function on every render of the component, e.g. to fetch data
     //useState is a hook which rerenders the component on every state change
+    // -->> changing the state inside use effect results in an infinite loop
     console.log("use effect ran");
-  });
+    setCounter(counter + 1);
 
+    // adding a dependency array ensures that it is just run the first time after the initial render
+    //further, adding values to the array ensures rendering again only if the added values changes
+  }, [name]);
 
   return (
     <div className="App">
@@ -31,6 +36,10 @@ function App() {
       <button onClick={countingDown}>Decrease Number</button>
       <p>{counter}</p>
       <button onClick={() => countingUp()}>Increase Number</button>
+      <p></p>
+      <p></p>
+      <p>{name}</p>
+      <button onClick={() => setName("Dora")}>Change Name</button>
     </div>
   );
 }
